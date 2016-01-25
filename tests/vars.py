2b9fe31a61431feecc165ba042d4b192aae28047
Rming <rmingwang@gmail.com>
2016-01-25 17:58:28 +0800

    在类属性内的表现

--- tests/vars.py
+++ tests/vars.py
@@ -20,3 +20,14 @@ print a  # [1]
 
 print fun.__doc__
 
+
+
+class Person:
+    name=[]
+
+p1=Person()
+p2=Person()
+p1.name.append(1)
+print p1.name  # [1]
+print p2.name  # [1]
+print Person.name  # [1]
